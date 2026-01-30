cfb6b7fa9b8937520a9c4437baf6c632a3e506c6
AYadrov <45910827+AYadrov@users.noreply.github.com>
2026-01-30 12:08:28 -0700

    Implement pow2 function
    Added Z3 function for power of 2 and properties.

--- fused_dot_product/numtypes/z3_utils.py
+++ fused_dot_product/numtypes/z3_utils.py
@@ -1,6 +1,13 @@
 from fractions import Fraction
-from z3 import If, RealVal
+from z3 import If, RealVal, Function, RealSort, ForAll, Real
 
+pow2 = Function("pow2", RealSort(), RealSort())
+x = Real('x')
+y = Real('y')
+pow2_props = [pow2(RealVal(0)) == RealVal(1), pow2(RealVal(1)) == RealVal(2), ForAll([x, y], pow2(x + y) == pow2(x) * pow2(y))]
+
+def pow_int_2(exp):
+    return pow2(exp)
 
 def pow2_int(exp, min_exp: int, max_exp: int):
     if min_exp > max_exp:
