a0b31b639c5f07dadfcba3264809db68fd2f5310
AYadrov <kowalski228@mail.ru>
2026-03-13 17:23:15 -0600

    run egglog iteratively

--- fused_dot_product/egglog/egraph.py
+++ fused_dot_product/egglog/egraph.py
@@ -16,27 +16,30 @@ def egglog_check_eq(ctx: "SpecContext", iterations=6):
     to_check = ctx.to_egglog(egraph)
     
     run_started_at = perf_counter()
-    run_report = egraph.run(iterations)
+    rule_application_counts: dict[str, int] = {}
+    iterations_used = 0
+    equivalent = egraph.check_bool(*to_check)
+
+    for _ in range(iterations):
+        if equivalent:
+            break
+        run_report = egraph.run(1)
+        iterations_used += 1
+        for rule, num_matches in run_report.num_matches_per_rule.items():
+            rule_name = str(rule)
+            rule_application_counts[rule_name] = (
+                rule_application_counts.get(rule_name, 0) + int(num_matches)
+            )
+        equivalent = egraph.check_bool(*to_check)
+
     run_runtime_s = perf_counter() - run_started_at
-    
-    rule_application_counts = {
-        str(rule): int(num_matches)
-        for rule, num_matches in run_report.num_matches_per_rule.items()
-    }
-    
-    equivalent = False
-    try:
-        for check in to_check:
-            egraph.check(check)
-        equivalent = True
-    except EggSmolError:
-        equivalent = False
     
     report = {
         "name": ctx.name,
         "equivalent": equivalent,
         "runtime_s": run_runtime_s,
         "rule_application_counts": rule_application_counts,
+        "iterations_used": iterations_used,
         "egraph": egraph,
     }
     
