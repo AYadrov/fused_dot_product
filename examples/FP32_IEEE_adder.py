52a2299f09e73d5e11c270ad791e51a2f551882d
AYadrov <kowalski228@mail.ru>
2026-03-31 18:16:41 -0600

    IEEE adder

---
+++ examples/FP32_IEEE_adder.py
@@ -0,0 +1,47 @@
+from fused_dot_product import *
+from .common import *
+from .encode_Float32 import *
+import random
+import struct
+
+import numpy as np
+
+Wf = 30
+
+@Composite(name="FP32_IEEE_adder", spec=lambda x, y, ctx: x + y)
+def FP32_IEEE_adder(x: Node, y: Node) -> Node:
+    # This follows the repo's current float model: finite, normalized inputs.
+    x_s, x_m, x_e = fp32_decode(x)
+    y_s, y_m, y_e = fp32_decode(y)
+
+    max_exp = uq_max(x_e, y_e)
+    x_shift_amount = uq_sub(max_exp, x_e)
+    y_shift_amount = uq_sub(max_exp, y_e)
+
+    x_m_implicit = mantissa_add_implicit_bit(x_m)
+    y_m_implicit = mantissa_add_implicit_bit(y_m)
+
+    # Make some room for right shift
+    x_m_resized = uq_resize(x_m_implicit, 1, Wf-1)
+    y_m_resized = uq_resize(y_m_implicit, 1, Wf-1)
+
+    x_m_shifted = uq_rshift(x_m_resized, x_shift_amount)
+    y_m_shifted = uq_rshift(y_m_resized, y_shift_amount)
+
+    x_m_q = uq_to_q(x_m_shifted)
+    y_m_q = uq_to_q(y_m_shifted)
+
+    x_m_signed = q_add_sign(x_m_q, x_s)
+    y_m_signed = q_add_sign(y_m_q, y_s)
+
+    m_sum = q_add(x_m_signed, y_m_signed)
+
+    return encode_Float32(m_sum, uq_to_q(max_exp))
+
+
+if __name__ == '__main__':
+    adder = FP32_IEEE_adder(
+        Var(name="a", sign=Float32T()),
+        Var(name="b", sign=Float32T()),
+    )
+    print(adder.check_spec())
--- fused_dot_product/components/Float.py
+++ fused_dot_product/components/Float.py
@@ -1,5 +1,49 @@
 from ..types import *
 from ..ast import *
+from .Tuple import make_Tuple
+
+########### Private Helpers ############
+
+def _fp32_mantissa(x: Node) -> Op:
+    def impl(x: Float32) -> UQ:
+        return UQ(x.mantissa, Float32.mantissa_bits, 0)
+    
+    def sign(x: Float32T) -> UQT:
+        return UQT(Float32.mantissa_bits, 0)
+    
+    return Op(
+            impl=impl,
+            sign=sign,
+            args=[x],
+            name="_fp32_mantissa")
+
+def _fp32_exponent(x: Node) -> Op:
+    def impl(x: Float32) -> UQ:
+        return UQ(x.exponent, Float32.exponent_bits, 0)
+    
+    def sign(x: Float32T) -> UQT:
+        return UQT(Float32.exponent_bits, 0)
+    
+    return Op(
+            impl=impl,
+            sign=sign,
+            args=[x],
+            name="_fp32_exponent")
+
+def _fp32_sign(x: Node) -> Op:
+    def impl(x: Float32) -> UQ:
+        return UQ(x.sign, 1, 0)
+    
+    def sign(x: Float32T) -> UQT:
+        return UQT(1, 0)
+    
+    return Op(
+            impl=impl,
+            sign=sign,
+            args=[x],
+            name="_fp32_sign")
+
+############## Public API ##############
 
 def float32_alloc(sign_bit: Node,
                    mantissa: Node,
@@ -15,3 +59,30 @@ def float32_alloc(sign_bit: Node,
         impl=impl,
         args=[sign_bit, mantissa, exponent],
         name="float32_alloc")
+
+def fp32_decode_spec(x, ctx):
+    sign = ctx.fresh_real("sign")
+    mantissa = ctx.fresh_real("mantissa")
+    exponent = ctx.fresh_real("exponent")
+
+    two = ctx.real_val(2)
+    one = ctx.real_val(1)
+    m_bits = ctx.real_val(Float32.mantissa_bits)
+    e_bits = ctx.real_val(Float32.exponent_bias)
+
+    mantissa_ = one + mantissa * two ** (-m_bits)
+    exponent_ = exponent - e_bits
+    ctx.assume(x.eq(sign * (mantissa_ * (two ** exponent_))))
+    ctx.assume(sign.eq(ctx.real_val(1)).or_(sign.eq(ctx.real_val(-1))))
+    ctx.assume((exponent >= ctx.real_val(0)).and_(exponent < ctx.real_val(1 << Float32.exponent_bits)))
+    ctx.assume((mantissa >= ctx.real_val(0)).and_(mantissa < ctx.real_val(1 << Float32.mantissa_bits)))
+    return (sign, mantissa, exponent)
+
+
+@Primitive(name="fp32_decode", spec=fp32_decode_spec)
+def fp32_decode(x: Node) -> Node:
+    return make_Tuple(
+        _fp32_sign(x),
+        _fp32_mantissa(x),
+        _fp32_exponent(x),
+    )
